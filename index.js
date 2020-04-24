8db4948e7218319e1422c74718c8e2d4da57605d
root <root@test-cicd2.c.thefthing-development.internal>
2020-04-24 03:33:29 +0000

    cuk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', (req, res) => {
-   res.send('<h1>Hello Dunia!</h1>')
+   res.send('<h1>Hello Cuk!</h1>')
 })
  
 const PORT = process.env.PORT || 8080
